e0020c965ab8ed6d5fc6ce58d8a4cd88299b0ce6
Steve-04 <vssteve004@gmail.com>
2023-04-16 17:06:10 +0530

    some changes in prompt

--- pages/api/generate.js
+++ pages/api/generate.js
@@ -10,8 +10,7 @@ const basePromptPrefix =
 Write me a detailed table of contents for the given subject on the given topic for a person who is of the given level in the topic. The table of contents should contain all the important points on the given topic. The table of contents should be enough for someone preparing for a big exam.
 Subject: 
 Topic: 
-Level: 
-Number of questions and answers: 
+Level:
 `
 ;
 const generateAction = async (req, res) => {
@@ -28,11 +27,10 @@ const generateAction = async (req, res) => {
   const basePromptOutput = baseCompletion.data.choices.pop();
 
   const secondPrompt = `
-  Take the table of contents and generate a very detailed study material. Give a very detailed explanation for each of the topics and their sub-topics. Give the respective heading before explaining each topic. The study material should be followed by the given number of questions and answers on the given topic. At the end of the study material give resources for further learning.
+  Take the table of contents and generate a very detailed study material. Give a very detailed explanation for each of the topics and their sub-topics. Give the respective heading before explaining each topic. The study material should be followed by questions and answers on the given topic. At the end of the study material give resources for further learning.
   Subject: ${req.body.userInput}
   Topic: ${req.body.userInput}
   Level: ${req.body.userInput}
-  Number of questions and answers: ${req.body.userInput}
   Table of Contents: ${basePromptOutput.text}
   Study Material:
   `

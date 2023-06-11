ff797cac11b02f32631dd2808411c59ffdbdcc6b
Steve-04 <vssteve004@gmail.com>
2023-06-11 18:48:04 +0530

    removed image generation(for now)

--- pages/index.js
+++ pages/index.js
@@ -65,55 +65,58 @@ const MyDocument = ({ apiOutput }) => {
 };
 
 const Home = () => {
-  const [userInput, setUserInput] = useState('');
-  const [apiOutput, setApiOutput] = useState('');
-  const [apiOutputImage, setApiOutputImage] = useState([]);
+  const [userInput, setUserInput] = useState("");
+  const [apiOutput, setApiOutput] = useState("");
+  // const [apiOutputImage, setApiOutputImage] = useState([]);
   const [conversation, setConversation] = useState([]);
   const [responseIndex, setResponseIndex] = useState(null);
   const [isGenerating, setIsGenerating] = useState(false);
-  const [isLoadingImages, setIsLoadingImages] = useState(false);
+  // const [isLoadingImages, setIsLoadingImages] = useState(false);
 
   // Load data from local storage on component mount
   useEffect(() => {
-    const storedApiOutput = localStorage.getItem('apiOutput');
-    const storedApiOutputImage = localStorage.getItem('apiOutputImage');
+    const storedApiOutput = localStorage.getItem("apiOutput");
+    // const storedApiOutputImage = localStorage.getItem('apiOutputImage');
 
     if (storedApiOutput) {
       setApiOutput(storedApiOutput);
     }
 
-    if (storedApiOutputImage) {
-      setApiOutputImage(JSON.parse(storedApiOutputImage));
-    }
+    // if (storedApiOutputImage) {
+    //   setApiOutputImage(JSON.parse(storedApiOutputImage));
+    // }
   }, []);
 
   // Save data to local storage when apiOutput or apiOutputImage changes
   useEffect(() => {
-    localStorage.setItem('apiOutput', apiOutput);
-    localStorage.setItem('apiOutputImage', JSON.stringify(apiOutputImage));
-  }, [apiOutput, apiOutputImage]);
+    localStorage.setItem("apiOutput", apiOutput);
+    // localStorage.setItem('apiOutputImage', JSON.stringify(apiOutputImage));
+  }, [apiOutput]); //[apiOutput, apiOutputImage]
 
   async function stream() {
     setIsGenerating(true);
-    setIsLoadingImages(true);
+    // setIsLoadingImages(true);
 
     setApiOutput("");
-    setApiOutputImage([]);
+    // setApiOutputImage([]);
     const updatedConversation = [
       ...conversation,
       { role: "user", content: userInput },
     ];
     setConversation(updatedConversation);
     setResponseIndex((prevIndex) => (prevIndex === null ? 1 : prevIndex + 2));
-  
+
     const response = await fetch("/api/generate", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
       },
-      body: JSON.stringify({ prompt: userInput, conversation: updatedConversation }),
+      body: JSON.stringify({
+        prompt: userInput,
+        conversation: updatedConversation,
+      }),
     });
-  
+
     const data = response.body;
     if (!data) {
       return;
@@ -129,31 +132,31 @@ const Home = () => {
       const chunkValue = decoder.decode(value);
       setApiOutput((prev) => prev + chunkValue);
     }
-    
-    const response2 = await fetch("/api/generateImage", {
-			method: "POST",
-			headers: {
-				"Content-Type": "application/json",
-        "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`
-			},
-			body: JSON.stringify({ userInput: userInput }),
-		});
 
-		const data2 = await response2.json();
-		const { output } = data2;
+    // const response2 = await fetch("/api/generateImage", {
+    //   method: "POST",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //     Authorization: `Bearer ${process.env.OPENAI_API_KEY}`,
+    //   },
+    //   body: JSON.stringify({ userInput: userInput }),
+    // });
 
-		setApiOutputImage(output);
+    // const data2 = await response2.json();
+    // const { output } = data2;
+
+    // setApiOutputImage(output);
 
     setIsGenerating(false);
-    setIsLoadingImages(false);
+    // setIsLoadingImages(false);
   }
 
   const onUserChangedText = (event) => {
-    const input1 = document.getElementById('input1').value;
-    const input2 = document.getElementById('input2').value;
-    const input3 = document.getElementById('input3').value;
+    const input1 = document.getElementById("input1").value;
+    const input2 = document.getElementById("input2").value;
+    const input3 = document.getElementById("input3").value;
     // const input4 = document.getElementById('input4').value;
-    const combinedInput = input1 + ' ' + input2 + ' ' + input3;
+    const combinedInput = input1 + " " + input2 + " " + input3;
 
     setUserInput(combinedInput);
   };
@@ -169,53 +172,62 @@ const Home = () => {
             <h1>Personal AI Tutor</h1>
           </div>
           <div className="header-subtitle">
-            <h2>Tell me which topic you want to learn and I will give a brief description followed by few questions and answers for you to revise!
-                <br></br>
-                <br></br>Subject: Enter your subject
-                <br></br>Topic: Enter your topic
-                <br></br>Level: beginner / intermediate / advanced / . . .
+            <h2>
+              Tell me which topic you want to learn and I will give a brief
+              description followed by few questions and answers for you to
+              revise!
+              <br></br>
+              <br></br>Subject: Enter your subject
+              <br></br>Topic: Enter your topic
+              <br></br>Level: beginner / intermediate / advanced / . . .
             </h2>
           </div>
         </div>
         <div className="prompt-container">
-          <textarea 
+          <textarea
             id="input1"
-            placeholder="subject" 
-            className="prompt-box" 
+            placeholder="subject"
+            className="prompt-box"
             // value={userInput}
             onChange={onUserChangedText}
           />
-          <textarea 
+          <textarea
             id="input2"
-            placeholder="topic" 
-            className="prompt-box" 
+            placeholder="topic"
+            className="prompt-box"
             // value={userInput}
             onChange={onUserChangedText}
           />
-          <textarea 
+          <textarea
             id="input3"
-            placeholder="level" 
-            className="prompt-box" 
+            placeholder="level"
+            className="prompt-box"
             // value={userInput}
             onChange={onUserChangedText}
           />
-          {/* <textarea 
-            id="input4"
-            placeholder="language" 
-            className="prompt-box" 
-            // value={userInput}
-            onChange={onUserChangedText}
-          /> */}
           <div className="prompt-buttons">
-            <a className="coffee" href="https://www.buymeacoffee.com/steve004" target="_blank">
-              <img src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png" alt="Buy Me A Coffee" />
+            <a
+              className="coffee"
+              href="https://www.buymeacoffee.com/steve004"
+              target="_blank"
+            >
+              <img
+                src="https://cdn.buymeacoffee.com/buttons/v2/default-yellow.png"
+                alt="Buy Me A Coffee"
+              />
             </a>
-            <a 
-              className={isGenerating ? 'generate-button loading' : 'generate-button'} 
+            <a
+              className={
+                isGenerating ? "generate-button loading" : "generate-button"
+              }
               onClick={stream}
             >
               <div className="generate">
-              {isGenerating ? <span className="loader"></span> : <p>Generate</p>}
+                {isGenerating ? (
+                  <span className="loader"></span>
+                ) : (
+                  <p>Generate</p>
+                )}
               </div>
             </a>
           </div>
@@ -227,17 +239,18 @@ const Home = () => {
                 </div>
               </div>
               <div className="output-content">
-                <ReactMarkdown 
+                <ReactMarkdown
+                  linkTarget="_blank"
                   children={apiOutput}
-                  remarkPlugins={[remarkGfm] [remarkMath]}
+                  remarkPlugins={[remarkGfm][remarkMath]}
                   rehypePlugins={[rehypeKatex]}
                   components={{
-                    code({node, inline, className, children, ...props}) {
-                      const match = /language-(\w+)/.exec(className || '')
+                    code({ node, inline, className, children, ...props }) {
+                      const match = /language-(\w+)/.exec(className || "");
                       return !inline && match ? (
                         <SyntaxHighlighter
                           {...props}
-                          children={String(children).replace(/\n$/, '')}
+                          children={String(children).replace(/\n$/, "")}
                           style={dark}
                           language={match[1]}
                           PreTag="div"
@@ -246,12 +259,12 @@ const Home = () => {
                         <code {...props} className={className}>
                           {children}
                         </code>
-                      )
-                    }
+                      );
+                    },
                   }}
                 />
               </div>
-              {isLoadingImages ? (
+              {/* {isLoadingImages ? (
                 <div className="loading-message">Generating images...</div>
               ) : (
                 <div className="image-section">
@@ -259,12 +272,12 @@ const Home = () => {
                     <img key={_index} src={image} />
                   ))}
                 </div>
-              )}
+              )} */}
               <PDFDownloadLink
                 document={<MyDocument apiOutput={apiOutput} />}
-                fileName={`${document.getElementById('input1').value}_${document.getElementById('input2').value}_${document.getElementById('input3').value}`}
+                fileName={`${document.getElementById("input1").value}_${document.getElementById("input2").value}_${document.getElementById("input3").value}`}
               >
-                <a className='download-button'>
+                <a className="download-button">
                   <div className="download">
                     <p>Download PDF</p>
                   </div>
